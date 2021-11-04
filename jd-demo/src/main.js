22f8737d0da0a370200368e6a1a56dcd8947fdce
Jamboy <jamboylive@gmail.com>
2021-11-04 14:48:53 +0800

    移动目录

--- jd-demo/src/main.js
+++ jd-demo/src/main.js
@@ -1,8 +1,15 @@
+/*
+ * @Description: 
+ * @Author: Jamboy
+ * @Date: 2021-09-13 13:51:39
+ * @LastEditTime: 2021-11-04 14:47:58
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'normalize.css'
 import '../style/index.scss'
+import '../style/base.scss'
 
 createApp(App).use(store).use(router).mount('#app')
